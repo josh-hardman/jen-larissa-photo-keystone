fc6ab8fe60d93cacfa49ff7737cc40433cf03da7
Joshua Hardman <jhardman0830@gmail.com>
2017-04-09 22:30:06 -0600

    fixing layout and adding scrollLock for the mobile navbar

--- public/js/nav.js
+++ public/js/nav.js
@@ -4,6 +4,6 @@ $( document ).ready( () => {
     $('.nav-toggle__open').toggleClass('nav-toggle__open--active')
     $('.nav-toggle__close').toggleClass('nav-toggle__close--active')
     $('.mobile-nav__background').toggleClass('mobile-nav__background--open')
-    $(body).toggleClass('lock-scroll')
+    $.scrollLock();
   })
 })
