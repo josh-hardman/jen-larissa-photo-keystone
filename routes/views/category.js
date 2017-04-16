6f2f1451ded7cca9fef0fd22b3eb97982336f613
Joshua Hardman <jhardman0830@gmail.com>
2017-04-16 13:12:12 -0600

    Adding ability to change portfolio section images

--- routes/views/category.js
+++ routes/views/category.js
@@ -16,21 +16,32 @@ exports = module.exports = function (req, res) {
   var PostCategory = keystone.list('PostCategory');
 
 	PostCategory.model.find()
-		.where('name', categoryName)
+		.where('name', categoryName + '-section')
     .exec(function(err, category) {
-			if( category[0] ) {
-				var id = category[0]._id;
-				view.query('galleries', keystone.list('Gallery').model.find(
-					{
-						categories: ObjectID(id)
+			var id = category[0]._id;
+			view.query('posts', keystone.list('Post').model.find(
+				{
+					categories: ObjectID(id)
+				}
+			).sort('sortOrder'));
+			PostCategory.model.find()
+				.where('name', categoryName)
+		    .exec(function(err, category) {
+					if( category[0] ) {
+						var id = category[0]._id;
+						view.query('galleries', keystone.list('Gallery').model.find(
+							{
+								categories: ObjectID(id)
+							}
+						).sort('sortOrder'));
+						view.render('category');
+					} else {
+						res.status(404).send(keystone.wrapHTMLError('Sorry, no page could be found at this address (404)'));
 					}
-				).sort('sortOrder'));
-				view.render('category');
-			} else {
-				res.status(404).send(keystone.wrapHTMLError('Sorry, no page could be found at this address (404)'));
-			}
-
+		    });
     });
+
+
 
 	// Load the galleries by sortOrder
 
